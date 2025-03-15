fb78b7163e8fcbbb23a5a78744a3947186e1e575
Michael Fransisco <michaelsinx.one@gmail.com>
2025-03-15 14:22:03 +0700

    gg

--- media/lua/client/PlayerKillTierHandler.lua
+++ media/lua/client/PlayerKillTierHandler.lua
@@ -3,72 +3,63 @@ require "PlayerKillCountServer"
 AlertHandler = {}
 
 function AlertHandler.addLineToChat(message, color, username, options)
-  if not isClient() then return end
+    if not isClient() then return end
 
-  if type(options) ~= "table" then
-      options = {
-          showTime = false,
-          serverAlert = false,
-          showAuthor = false,
-      }
-  end
-
-  if type(color) ~= "string" then
-      color = "<RGB:1,1,1>"
-  end
-
-  if options.showTime then
-      local dateStamp = Calendar.getInstance():getTime()
-      local dateFormat = SimpleDateFormat.new("H:mm")
-      if dateStamp and dateFormat then
-          message = color .. "[" .. tostring(dateFormat:format(dateStamp) or "N/A") .. "]  " .. message
-      end
-  else
-      message = color .. message
-  end
-
-  local msg = {
-      getText = function(_)
-          return message
-      end,
-      getTextWithPrefix = function(_)
-          return message
-      end,
-      isServerAlert = function(_)
-          return options.serverAlert
-      end,
-      isShowAuthor = function(_)
-          return options.showAuthor
-      end,
-      getAuthor = function(_)
-          return tostring(username)
-      end,
-      setShouldAttractZombies = function(_)
-          return false
-      end,
-      setOverHeadSpeech = function(_)
-          return false
-      end,
-  }
-
-  if not ISChat.instance then return end
-  if not ISChat.instance.chatText then return end
-  ISChat.addLineInChat(msg, 0)
-end
-
-local function OnServerCommand(module, command, arguments)
-  local players = getOnlinePlayers()
-    if players then
-        for i = 0, players:size() - 1 do
-            local player = players:get(i)
-            local username = player:getUsername()
-            local killCount = PlayerKillCountServer.loadKillCountFromFile(username)
-            -- Check if the player has reached or exceeded 1000 kills
-            local message = "Congratulations " .. username .. " ! You have reached- " .. killCount .. " -zombie kills!"
-            AlertHandler.addLineToChat(message,"<RGB:255,0,0>", username)
-        end
+    if type(options) ~= "table" then
+        options = {
+            showTime = false,
+            serverAlert = false,
+            showAuthor = false,
+        }
     end
 
+    if type(color) ~= "string" then
+        color = "<RGB:1,1,1>"
+    end
+
+    if options.showTime then
+        local dateStamp = Calendar.getInstance():getTime()
+        local dateFormat = SimpleDateFormat.new("H:mm")
+        if dateStamp and dateFormat then
+            message = color .. "[" .. tostring(dateFormat:format(dateStamp) or "N/A") .. "]  " .. message
+        end
+    else
+        message = color .. message
+    end
+
+    local msg = {
+        getText = function(_)
+            return message
+        end,
+        getTextWithPrefix = function(_)
+            return message
+        end,
+        isServerAlert = function(_)
+            return options.serverAlert
+        end,
+        isShowAuthor = function(_)
+            return options.showAuthor
+        end,
+        getAuthor = function(_)
+            return tostring(username)
+        end,
+        setShouldAttractZombies = function(_)
+            return false
+        end,
+        setOverHeadSpeech = function(_)
+            return false
+        end,
+    }
+
+    if not ISChat.instance then return end
+    if not ISChat.instance.chatText then return end
+    ISChat.addLineInChat(msg, 0)
 end
--- Register the function to be called every in-game day
--- Events.EveryDays.Add(OnServerCommand)
+
+local function OnServerCommand(module, command, args)
+    if module == "ServerAlert" and command == "alert" then
+        AlertHandler.addLineToChat(args.message, args.color, args.username, args.options)
+    end
+end
+
+Events.OnServerCommand.Add(OnServerCommand)
--- media/lua/server/ServerAlert.lua
+++ media/lua/server/ServerAlert.lua
@@ -1,15 +1,33 @@
-function SendAlert()
+function SendAlert(targetPlayer)
   local players = getOnlinePlayers()
   if players then
       for i = 0, players:size() - 1 do
           local player = players:get(i)
-          sendServerCommand(player, "ServerAlert", "alert", { name = "TEST NAME" })
+          if not targetPlayer or player == targetPlayer then
+              sendServerCommand(player, "ServerAlert", "alert", {
+                  message = "This is a test alert.",
+                  color = "<RGB:1,0,0>", -- Red color
+                  username = "Server",
+                  options = {
+                      showTime = true,
+                      serverAlert = true,
+                      showAuthor = true,
+                  }
+              })
+          end
       end
   end
 end
 
 local function OnClientCommand(module, command, player, args)
-    SendAlert()
+  if module == "ServerAlert" and command == "sendAlert" then
+      if args.targetPlayer then
+          local targetPlayer = getPlayerByUsername(args.targetPlayer)
+          SendAlert(targetPlayer)
+      else
+          SendAlert()
+      end
+  end
 end
 
 Events.OnClientCommand.Add(OnClientCommand)
