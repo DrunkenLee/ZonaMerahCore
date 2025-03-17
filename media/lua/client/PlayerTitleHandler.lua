a4c43ad7f4802957b57314f58eee595094d1e79e
Michael Fransisco <michaelsinx.one@gmail.com>
2025-03-17 15:50:31 +0700

    title handler client only

--- media/lua/client/PlayerTitleHandler.lua
+++ media/lua/client/PlayerTitleHandler.lua
@@ -1,32 +1,19 @@
 PlayerTitleHandler = {}
-PlayerTitleHandler.storedTitles = {}
 
--- Function to get the player's title from the server
-function PlayerTitleHandler.getPlayerTitle(player, callback)
+local titlesValue = {1, 2, 3}
+-- 1 = VIP, 2 = VVIP, 3 = MVP
+
+function PlayerTitleHandler.assignPlayerTitle(player, title)
     local username = player:getUsername()
-
-    -- Send a request to the server to get the player's title
-    sendClientCommand("PlayerTitleHandlerServer", "getTitle", { username = username })
-    -- print("Sent request to server for title")
-
-    -- Listen for the server's response
-    local function onServerCommand(module, command, args)
-        if module == "PlayerTitleHandlerServer" and command == "sendTitle" and args.username == username then
-            local title = args.title or "DEFAULT"
-            -- print("Received title from server: " .. title .. " for " .. username)
-            Events.OnServerCommand.Remove(onServerCommand)
-            PlayerTitleHandler.storedTitles[username] = title
-            callback(title) -- Call the callback function with the title
-        end
-    end
-
-    Events.OnServerCommand.Add(onServerCommand)
+    PlayerTitleHandler[username] = title
+    player:Say("Your grade has been upgraded successfully: " .. title)
 end
 
--- Function to retrieve the stored title for a player
-function PlayerTitleHandler.getStoredTitle(player)
+function PlayerTitleHandler.getPlayerTitle(player)
     local username = player:getUsername()
-    return PlayerTitleHandler.storedTitles[username] or "DEFAULT"
+    local title = PlayerTitleHandler[username] or 0
+    print("[ZonaMerahCore] Player: " .. username .. " has title: " .. title)
+    return title
 end
 
 return PlayerTitleHandler
