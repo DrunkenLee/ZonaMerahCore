b57a1ba23bdd54989b17973ff6ef7933391a835f
Michael Fransisco <michaelsinx.one@gmail.com>
2025-03-20 11:23:29 +0700

    gg main

--- media/lua/server/testserver.lua
+++ media/lua/server/testserver.lua
@@ -1,5 +1,5 @@
 Events.OnClientCommand.Add(function(module, command, player, args)
-  print("[TestServer] Received command from client: " .. command)
+  -- print("[TestServer] Received command from client: " .. command)s
   if module == "TestModule" and command == "TestCommand" then
       print("[TestServer] Received command from client: " .. args.message)
       print("[TestServer] Sending response back to client...")
